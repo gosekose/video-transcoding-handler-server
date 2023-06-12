43b59a3865d3c90d8ecf8452adfb4ba9201b1473
gosekose <ksyn1611@naver.com>
2023-06-12 16:32:39 +0900

    feat: [테스트 필요] flyway 초기화 완료

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,41 @@
+alter table video_metadata
+
+drop
+foreign key FKccwcre7a2ik4379h39pen4rx5;
+
+drop table if exists video_description_seq;
+drop table if exists video_metadata_seq;
+drop table if exists video_description;
+drop table if exists video_metadata;
+
+create table video_description_seq (
+    next_val bigint
+) engine=InnoDB;
+
+insert into video_description_seq values ( 1 );
+
+create table video_metadata_seq (
+    next_val bigint
+) engine=InnoDB;
+
+insert into video_metadata_seq values ( 1 );
+
+create table video_description (
+   video_information_id bigint not null,
+   primary key (video_information_id)
+) engine=InnoDB;
+
+create table video_metadata (
+    video_description_id bigint,
+    video_metadata_id bigint not null,
+    bitrate varchar(255),
+    duration varchar(255),
+    file_path varchar(255),
+    format varchar(255),
+    primary key (video_metadata_id)
+) engine=InnoDB;
+
+alter table video_metadata
+    add constraint FKccwcre7a2ik4379h39pen4rx5
+        foreign key (video_description_id)
+            references video_description (video_information_id);
